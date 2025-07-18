8fdfa8eec726bec65b16966f9b174b9ac05c7598
daisydaisyyy <giulia.aloia05@gmail.com>
2025-07-18 19:41:49 +0200

    Minor bugs fixing

--- db/connection.php
+++ db/connection.php
@@ -2,8 +2,8 @@
 function getPDO() {
     $host = 'localhost';
     $db   = 'plantsim';
-    $user = 'dbuser';
-    $pass = 'dbpass';
+    $user = 'root';
+    $pass = '';
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
     $opts = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
